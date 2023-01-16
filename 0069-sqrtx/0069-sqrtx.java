3c189a6147a1234e4badea6e3d327acee32f78cc
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-01-16 15:25:00 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (77.31%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)(Math.sqrt(x));
+        // return (int)(Math.sqrt(x));
+        long low = 1;
+        long high = x;
+        long ans = 0;
+        while(low <= high){
+            long mid = low + (high - low)/2;
+            if(mid * mid <= x){
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return (int)(ans);
     }
 }
