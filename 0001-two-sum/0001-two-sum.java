8cd50d7aff256f82246402e6e9d908262a8aefa6
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-01-24 20:40:02 +0530

    Time: 4 ms (73.95%), Space: 45.4 MB (19.01%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //Map m daal do and target m se minus krdo ... agr iske baad num milega map m to un dono ka array bnake ke return krdo nhi to null return krdo
+        HashMap<Integer,Integer> helper = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            Integer num = (Integer)(target - nums[i]);
+            if(helper.containsKey(num)){
+                int[] ans = {helper.get(num),i};
+                return ans;
+            }
+            helper.put(nums[i],i);
+        }
+        return null;
+    }
+}
