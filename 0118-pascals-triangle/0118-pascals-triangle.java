8c9b851b15720053a7e414dd8d87a9e4ca9a0dc1
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-01-25 10:53:31 +0530

    Time: 1 ms (68.88%), Space: 43 MB (5.86%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        if(numRows == 0) return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=1;i<=numRows;i++){
+            List<Integer> helper = new ArrayList<>();
+            for(int j=0;j<i;j++){
+                if(j==0 || j==i-1){
+                    helper.add(1);
+                }else{
+                    helper.add(ans.get(i-2).get(j-1) + ans.get(i-2).get(j)); // i-2 because we started from but the index is 0 based.
+                }
+            }
+            ans.add(helper);
+        }
+        return ans;
+    }
+}
