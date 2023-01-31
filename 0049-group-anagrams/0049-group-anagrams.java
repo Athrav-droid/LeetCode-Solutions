2d881f2f144ce6c9ef6c63fe26db21a71b559ea8
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-01-31 22:03:30 +0530

    Time: 7 ms (84.57%), Space: 45.5 MB (75.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,37 +1,55 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-        boolean[] check = new boolean[strs.length];
-        for(int i=0;i<check.length;i++){
-            check[i] = false;
+    //USING HASHMAP
+    Map<String,List<String>> map = new HashMap<>();
+    for(int i=0;i<strs.length;i++){
+        String curr = strs[i];
+        char[] currArr = curr.toCharArray();
+        Arrays.sort(currArr);
+        String newCurr = new String(currArr);
+        if(map.containsKey(newCurr)){
+            map.get(newCurr).add(curr);
+        }else{
+            map.put(newCurr,new ArrayList<>());
+            map.get(newCurr).add(curr);
         }
-        for(int i=0;i<strs.length;i++){
-            String curr = strs[i];
-            List<String> helper = new ArrayList<>();
-            if(check[i]) continue;
-            for(int j=i;j<strs.length;j++){
-                // if(check[j]) break;
-                if(isAnagram(curr,strs[j])){
-                    check[j] = true;
-                    helper.add(strs[j]);
-                }
-            }
-            ans.add(helper);
-        }
-        return ans;
     }
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        int[] arr = new int[26];
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
-        }
-        for(int val: arr){
-            if(val!=0){
-                return false;
-            }
-        }
-        return true;
+    return new ArrayList<>(map.values());
+
+    //BRUTE FORCE    
+    //     List<List<String>> ans = new ArrayList<>();
+    //     boolean[] check = new boolean[strs.length];
+    //     for(int i=0;i<check.length;i++){
+    //         check[i] = false;
+    //     }
+    //     for(int i=0;i<strs.length;i++){
+    //         String curr = strs[i];
+    //         List<String> helper = new ArrayList<>();
+    //         if(check[i]) continue;
+    //         for(int j=i;j<strs.length;j++){
+    //             // if(check[j]) break;
+    //             if(isAnagram(curr,strs[j])){
+    //                 check[j] = true;
+    //                 helper.add(strs[j]);
+    //             }
+    //         }
+    //         ans.add(helper);
+    //     }
+    //     return ans;
+    // }
+    // public boolean isAnagram(String s, String t) {
+    //     if(s.length()!=t.length()) return false;
+    //     int[] arr = new int[26];
+    //     for(int i=0;i<s.length();i++){
+    //         arr[s.charAt(i)-'a']++;
+    //         arr[t.charAt(i)-'a']--;
+    //     }
+    //     for(int val: arr){
+    //         if(val!=0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     }
 }
