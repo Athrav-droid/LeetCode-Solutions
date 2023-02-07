0b006efa7abda7ddc5c67ca59b70da253158b219
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-02-07 09:26:51 +0530

    Time: 1182 ms (8.92%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,11 @@
 # Write your MySQL query statement below
-select customer_number from orders group by customer_number having count(customer_number) >= all(select count(customer_number) from orders group by customer_number)
+
+# select customer_number from orders group by customer_number having count(customer_number) >= all(select count(customer_number) from orders group by customer_number)
+
+SELECT
+    customer_number
+FROM
+    orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
