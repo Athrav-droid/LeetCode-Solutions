0c8fa3e3fae86bd2c2203b8fd81f4d83b3343c3e
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-02-15 19:52:04 +0530

    Time: 1 ms (88.86%), Space: 42.6 MB (41.33%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -14,17 +14,11 @@ class Solution {
         if(nums.length == 1) return nums[0];
         int max = nums[0], min = nums[0], ans = nums[0];
         for(int i=1;i<nums.length;i++){
-            System.out.println(max);
-            System.out.println(min);
-            System.out.println("----");
             if(nums[i] < 0){
                 int temp = max;
                 max = min;
                 min = temp;
             } // swapping when negative coz it -ve number will convert max into min and min to max for that particular loop 
-            System.out.println(max);
-            System.out.println(min);
-            System.out.println("----");
             
             max = Math.max(nums[i], max * nums[i]); // two possiblities 1. you are starting a new subarray i.e nums[i] 2. continuing the old array i.e nums[i] * max
             min = Math.min(nums[i], min * nums[i]);
