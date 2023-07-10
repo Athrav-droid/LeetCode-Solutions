9d96c003a059cb957d18b3b42fef54e2c7570a44
Athrav Mehta <81102465+Athrav-droid@users.noreply.github.com>
2023-07-10 16:31:11 +0530

    Time: 1888 ms (40.00%), Space: 58.3 MB (40.00%) - LeetHub

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkArray(int[] nums, int k) {
+        int n = nums.length;
+        int curr = 0;
+        for(int i = 0; i <= n - k; i++){
+            if(nums[i] == 0) continue;
+            if(nums[i] < 0) return false;
+            if(nums[i] > 0) curr = nums[i];
+            for(int j = i; j < i + k; j++){
+                nums[j] -= curr;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0) return false;
+        }
+        return true;
+    }
+}
